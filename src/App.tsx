3ad9144c7dd332ca655d2596cccbfdbb63cd02b4
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-10-09 10:37:07 +0300

    change import Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/header/Header";
-import {Main} from "./components/presentation/Main";
+import {Main} from "./components/main/Main";
 import {Footer} from "./components/footer/Footer";
 
 
