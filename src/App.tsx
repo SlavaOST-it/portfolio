088df95eabf088cb35e12dc208bc0a79b9202b5d
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-10-09 10:25:34 +0300

    add Header, Main, Footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/header/Header";
+import {HashRouter} from "react-router-dom";
+import {Main} from "./components/presentation/Main";
+import {Footer} from "./components/footer/Footer";
+
 
 function App() {
     return (
         <div className="App">
-            <Header/>
+            <HashRouter>
+                <Header/>
+                <Main/>
+                <Footer/>
+            </HashRouter>
         </div>
     );
 }
