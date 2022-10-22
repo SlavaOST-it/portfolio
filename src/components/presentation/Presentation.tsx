b367c1338dddfb0d9ed9c658f8af1f1a707b95a4
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-10-22 16:15:45 +0300

    add effect writer text

--- src/components/presentation/Presentation.tsx
+++ src/components/presentation/Presentation.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import s from "./Presentation.module.css"
 import photo from "../../common/img/me.jpg"
+import {useTypewriter, Cursor} from "react-simple-typewriter";
 
 
 export const Presentation = () => {
+    const [text] = useTypewriter({
+        words: ['Front-end Developer'],
+        typeSpeed: 150,
+        loop: true,
+    })
+
     return (
         <div className={s.presentationBlock}>
             <div className={s.photoBlock}>
@@ -15,7 +22,11 @@ export const Presentation = () => {
                     <div className={s.lastName}>OSTAPKEVICH</div>
                 </div>
                 <p className={s.hr}/>
-                <div className={s.text}>Front-end Developer</div>
+
+                <div className={s.text}>
+                    <span>{text}</span>
+                    <Cursor/>
+                </div>
             </div>
         </div>
     )
